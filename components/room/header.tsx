c813b82393be9c5435656655a780dbaee19576b1
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-12-01 15:06:34 +0530

    add leave room

--- components/room/header.tsx
+++ components/room/header.tsx
@@ -1,7 +1,9 @@
+'use client';
 import ShareButton from '@/components/share-button';
 import { Button } from '@/components/ui/button';
 import UserProfileDropdown from '@/components/user-profile-dropdown';
 import { DoorOpenIcon, Music2Icon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 const RoomHeader = ({
   roomName,
@@ -10,6 +12,8 @@ const RoomHeader = ({
   roomName: string;
   pathname: string;
 }) => {
+  const router = useRouter();
+
   return (
     <header className="sticky left-0 right-0 top-0 z-20 border-b bg-background">
       <div className="flex h-16 items-center justify-between py-3 mx-auto w-full max-w-screen-lg px-3 lg:px-20">
@@ -27,6 +31,9 @@ const RoomHeader = ({
           <Button
             variant={'destructive'}
             className="text-destructive bg-destructive/10 hover:bg-destructive/20"
+            onClick={() => {
+              router.replace('/room');
+            }}
           >
             <DoorOpenIcon />{' '}
             <span className="hidden md:inline-block">Leave Room</span>
